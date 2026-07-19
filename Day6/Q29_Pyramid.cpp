7342392c8acf6b3ba4fca395bae3760e224b1d18
IRSHANAEEM <Alexeous151@gmail.com>
2026-07-19 23:04:58 +0500

    Day6:Completed beginner C++ Program

---
+++ Day6/Q29_Pyramid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the number: ";
+cin>>n;
+for(int i=1;i<=n;i++){
+ for(int a=n-1;a>=i;a--){
+cout<<" ";
+}
+for(int j=1;j<=i;j++){
+cout<<"*";
+cout<<" ";
+}
+cout<<endl;
+}
+}
---
+++ Day6/Q30_FloydsTriangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n=1;
+for(int j=1;j<=5;j++){
+for(int i=1;i<=j;i++){
+cout<<n<<" ";
+n++;
+}
+cout<<endl;
+}}
+
